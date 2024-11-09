69d68835c838238652364e9e48b85c7a475ce812
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2024-11-09 13:29:35 +0530

    Time: 2 ms (99.5%), Space: 48.2 MB (20.48%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,37 @@
 class Solution {
-    public List<Integer> majorityElement(int[] arr) {
+    public List<Integer> majorityElement(int[] nums) {
+        // Step 1: Identify potential candidates using Boyer-Moore Voting Algorithm
+        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
         
-        HashMap<Integer,Integer> map = new HashMap<>();
-
-        for(int i=0; i<arr.length; i++)
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
-
-        
-        ArrayList<Integer> list = new ArrayList<>();
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-
-            if(entry.getValue()>arr.length/3)
-                list.add(entry.getKey());
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
         }
-        return list;
+        
+        // Step 2: Validate the candidates by counting their occurrences
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        if (count1 > nums.length / 3) result.add(candidate1);
+        if (count2 > nums.length / 3) result.add(candidate2);
+        
+        return result;
     }
 }
