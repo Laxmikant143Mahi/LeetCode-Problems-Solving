c409061befcdcca195fdb1b1ff1c789b243b31e1
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2024-12-16 12:20:02 +0530

    Time: 18 ms (11.5%), Space: 45.7 MB (33.54%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -4,6 +4,7 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
         int duplicate = -1, missing = -1;
+        int n = nums.length;
 
         // Count the frequency of each number in the array
         for (int num : nums) {
@@ -11,7 +12,7 @@ class Solution {
         }
 
         // Check the numbers from 1 to n
-        for (int i = 1; i <= nums.length; i++) {
+        for (int i = 1; i <= n; i++) {
             if (map.containsKey(i)) {
                 if (map.get(i) == 2) {
                     duplicate = i; // This number is duplicated
