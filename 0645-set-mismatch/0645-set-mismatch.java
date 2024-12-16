ceecf371c8540f5888e261f1e2d215277551827a
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2024-12-16 12:16:41 +0530

    Time: 18 ms (11.5%), Space: 46.3 MB (6.95%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,26 @@
+
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int duplicate = -1, missing = -1;
+
+        // Count the frequency of each number in the array
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Check the numbers from 1 to n
+        for (int i = 1; i <= nums.length; i++) {
+            if (map.containsKey(i)) {
+                if (map.get(i) == 2) {
+                    duplicate = i; // This number is duplicated
+                }
+            } else {
+                missing = i; // This number is missing
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
