9044b8d2de82f58dfc3377cdc40c2dbb3ecd9f6d
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2024-12-25 20:38:56 +0530

    Time: 163 ms (45.12%), Space: 41.3 MB (35.74%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,74 +1,73 @@
 class Solution {
 
-    public boolean canPlaceDigit(char[][] board, int row, int col, char digit){
-
-        for(int i=0; i<9;i++ ){
-
-            if(board[row][i] == digit){
+    // Checks if a digit can be placed at board[row][col] without violating Sudoku rules
+    public boolean canPlaceDigit(char[][] board, int row, int col, char digit) {
+        // Check row
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == digit) {
                 return false;
             }
         }
 
-        for(int i=0; i<9; i++){
-
-            if(board[i][col] == digit){
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == digit) {
                 return false;
             }
         }
 
-        int startRow = (row/3)*3;
-        int startCol = (col/3)*3;
+        // Check 3x3 subgrid
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
 
-
-        for(int i=startRow; i<=startRow+2; i++){
-
-            for(int j=startCol; j<=startCol+2; j++){
-
-                if(board[i][j] == digit){
+        for (int i = startRow; i <= startRow + 2; i++) {
+            for (int j = startCol; j <= startCol + 2; j++) {
+                if (board[i][j] == digit) {
                     return false;
                 }
             }
         }
         return true;
-
     }
-    public boolean sudokuHelper(char[][] board,  int row, int col){
 
-        if(row==9)
+    // Solves the Sudoku board using backtracking
+    public boolean sudokuHelper(char[][] board, int row, int col) {
+        // If all rows are processed, the board is solved
+        if (row == 9)
             return true;
-        
+
+        // Move to the next cell
         int nextRow = row;
-        int nextCol = col+1;
+        int nextCol = col + 1;
 
-        if(nextCol == 9){
-            nextRow = nextRow+1;
-            nextCol = 0 ;
+        // If the end of the column is reached, move to the next row
+        if (nextCol == 9) {
+            nextRow = nextRow + 1;
+            nextCol = 0;
         }
 
-
-        if(board[row][col] != '.'){
-            
-           return sudokuHelper(board,nextRow, nextCol);
+        // If the cell is already filled, skip it
+        if (board[row][col] != '.') {
+            return sudokuHelper(board, nextRow, nextCol);
         }
 
-        for(char digit = '1'; digit<='9'; digit++){
+        // Try placing digits 1-9 and check validity
+        for (char digit = '1'; digit <= '9'; digit++) {
+            if (canPlaceDigit(board, row, col, digit)) {
+                board[row][col] = digit; // Place digit
 
-            if(canPlaceDigit(board,row,col,digit)){
-                board[row][col] = digit;
-
-                if(sudokuHelper(board,nextRow, nextCol))
+                // Recur to solve the rest of the board
+                if (sudokuHelper(board, nextRow, nextCol))
                     return true;
-                
-                board[row][col] = '.';
+
+                board[row][col] = '.'; // Backtrack if placement is invalid
             }
-
         }
-        return false;
-
-
+        return false; // No valid placement found
     }
+
+    // Main function to solve the Sudoku board
     public void solveSudoku(char[][] board) {
-        
-        sudokuHelper(board,0,0);
+        sudokuHelper(board, 0, 0);
     }
 }
