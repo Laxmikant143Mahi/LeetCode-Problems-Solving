a04cb0cf16064b9e4133f9ed5bbe0a6ff946bf2a
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2025-01-05 06:03:13 +0300

    Time: 2 ms (99.6%), Space: 47.1 MB (53.87%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
@@ -1,35 +1,18 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
-        int i = 0; // Pointer for even index
-        int j = 1; // Pointer for odd index
-        int n = nums.length;
-
-        while (i < n && j < n) {
-            // If i points to an even index but contains an odd number
-            if (i % 2 == 0 && nums[i] % 2 != 0) {
-                // Swap nums[i] and nums[j]
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+        int even = 0;
+        int odd = 1;
+        int ans[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                ans[even] = nums[i];
+                even+=2;
             }
-
-            // If j points to an odd index but contains an even number
-            if (j % 2 == 1 && nums[j] % 2 != 1) {
-                // Swap nums[j] and nums[i]
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-            }
-
-            // Move pointers to the next positions
-            if (nums[i] % 2 == 0) {
-                i += 2; // Move to the next even index
-            }
-            if (nums[j] % 2 == 1) {
-                j += 2; // Move to the next odd index
+            else{
+                ans[odd] = nums[i];
+                odd+=2;
             }
         }
-
-        return nums;
+        return ans;
     }
 }
