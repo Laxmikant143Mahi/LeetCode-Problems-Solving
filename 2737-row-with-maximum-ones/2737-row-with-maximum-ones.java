e8d10a3e6a444b62576cedd58df3ffdc9f971fac
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2025-01-15 19:55:03 +0300

    Time: 4 ms (81.94%), Space: 46.3 MB (91.03%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.java
@@ -3,10 +3,15 @@ class Solution {
         
         int maxCount = Integer.MIN_VALUE;
         int index=-1;
-        for(int i=0; i<mat.length; i++){
+
+        int row  = mat.length;
+        int col = mat[0].length;
+
+
+        for(int i=0; i<row; i++){
             
             int count=0;
-            for(int j=0; j<mat[0].length; j++){
+            for(int j=0; j<col; j++){
 
                 if(mat[i][j] == 1)
                     count++;
