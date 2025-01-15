f239b0853dbaf12503bde982739d2b3124809210
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2025-01-15 19:55:26 +0300

    Time: 4 ms (81.94%), Space: 46.9 MB (8.97%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.java
@@ -7,10 +7,10 @@ class Solution {
         int row  = mat.length;
         int col = mat[0].length;
 
-
+        int count=0;
         for(int i=0; i<row; i++){
             
-            int count=0;
+            count=0;
             for(int j=0; j<col; j++){
 
                 if(mat[i][j] == 1)
