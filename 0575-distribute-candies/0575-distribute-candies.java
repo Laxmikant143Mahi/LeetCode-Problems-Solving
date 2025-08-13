731261ef359c41da4de668b63d11aaaef1fa7d60
Laxmikant Mahindrakar <111036655+Laxmikant143Mahi@users.noreply.github.com>
2025-08-13 21:28:54 +0530

    Time: 39 ms (9.99%), Space: 46.5 MB (8.97%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.java
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -1,16 +1,15 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
-        
+        int n = candyType.length;
+        int canEat = n/2;
+
         HashMap<Integer,Integer> map = new HashMap<>();
 
-        int n = candyType.length;
         for(int i=0; i<n; i++){
+
             map.put(candyType[i], map.getOrDefault(candyType[i],0)+1);
         }
 
-        int possibleCandies = map.size();
-
-        if(possibleCandies>=(n/2)) return n/2;
-        else return possibleCandies;
+        return Math.min(canEat,map.size());
     }
 }
